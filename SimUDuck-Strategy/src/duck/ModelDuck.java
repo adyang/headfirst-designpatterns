962163926d8804ff959dc22c02dae7233084dbe0
adyang <adyang2011@gmail.com>
2017-07-17 00:49:36 +0800

    Add SimUDuck Strategy Pattern

---
+++ SimUDuck-Strategy/src/duck/ModelDuck.java
@@ -0,0 +1,18 @@
+package duck;
+
+import behavior.FlyNoWay;
+import behavior.Quack;
+
+public class ModelDuck extends Duck {
+
+	public ModelDuck() {
+		flyBehavior = new FlyNoWay();
+		quackBehavior = new Quack();
+	}
+
+	@Override
+	public void display() {
+		System.out.println("I'm a model duck");
+	}
+
+}

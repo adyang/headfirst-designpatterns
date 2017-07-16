cfacb9d3ef0bfd70d94238831b1c50b930ea5242
adyang <adyang2011@gmail.com>
2017-07-17 00:53:51 +0800

    Add Starbuzz Decorator pattern

---
+++ Starbuzz-Decorator/src/beverage/condiment/Soy.java
@@ -0,0 +1,21 @@
+package beverage.condiment;
+
+import beverage.Beverage;
+
+public class Soy extends CondimentDecorator {
+	private Beverage beverage;
+
+	public Soy(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	@Override
+	public String getDescription() {
+		return beverage.getDescription() + ", Soy";
+	}
+
+	@Override
+	public double cost() {
+		return 0.15 + beverage.cost();
+	}
+}
---
+++ Starbuzz-Decorator/src/beverage/condiment/SteamedMilk.java
@@ -0,0 +1,21 @@
+package beverage.condiment;
+
+import beverage.Beverage;
+
+public class SteamedMilk extends CondimentDecorator {
+	private Beverage beverage;
+
+	public SteamedMilk(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	@Override
+	public String getDescription() {
+		return beverage.getDescription() + ", Steamed Milk";
+	}
+
+	@Override
+	public double cost() {
+		return 0.10 + beverage.cost();
+	}
+}
